08c7f17c9ea8b0f75b76e6725b54040955218f28
tmntr <tomnoy0809@highgateschool.org.uk>
2026-09-08 15:07:55 +0100

    Almost there. just need to understand the complex output of fft

--- FFT basic functions.py
+++ FFT basic functions.py
@@ -4,44 +4,51 @@ import pyaudio
 import random
 
 
-p = pyaudio.PyAudio()
+def alterValueByDecibels(value, decibels):
+    return value*10**(decibels/10)
 
+#p = pyaudio.PyAudio()
 Fs = 441000
 T = 1/Fs
-t = np.arange(0, 1, T)
 
+t = np.arange(0, 1, T)
 f1 = 50
 signal = 0.7*np.sin(2*np.pi*f1*t)
+
+
+
 for i in range(15):
     signal = signal + random.random() * np.sin(2*np.pi*random.randint(1,10000)*t)
-
-
-
 fft_values = np.fft.fft(signal)
+
 N = len(signal)
 
 freq = np.fft.fftfreq(N, T)
 
+
+
+
+new_fft_values = fft_values
+
+
 magnitude = np.abs(fft_values)
 
-
-
-new_sample = np.fft.ifft(fft_values)
-
-
-
 for i in range(len(freq)//2):
-    print(f"{freq[i]:.2f} Hz, {magnitude[i]:.2f}")
+    if 2000 < freq[i] < 5000:
+        newreal = alterValueByDecibels(new_fft_values[i].real, 17)
+        newimaginary = new_fft_values[i].imag
+        new_fft_values[i] = newreal + newimaginary * 1.0j
+
+new_sample = np.abs(np.fft.ifft(new_fft_values))
+print(signal)
 
 
 
-def alterValueByDecibels(value, decibels):
-    return value*10**(decibels/10)
+
+print(new_sample)
 
 
-#for i in range(len(freq)//2):
-#    if 2000 < freq[i] < 5000:
-#  magnitude[i] = alterValueByDecibels(magnitude[i], 17)
+
 
 
 
